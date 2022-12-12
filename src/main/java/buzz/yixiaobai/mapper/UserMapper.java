afad6077a7b150b6b364dd75ae95bd43d1ab782b
YiXiaoBai <yixiaobai.lp@qq.com>
2022-12-12 23:56:11 +0800

    MyBatisPlus Wrapper 的使用 条件构建器的使用

--- src/main/java/buzz/yixiaobai/mapper/UserMapper.java
+++ src/main/java/buzz/yixiaobai/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package buzz.yixiaobai.mapper;
 
 import buzz.yixiaobai.pojo.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.MapKey;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
@@ -9,6 +10,7 @@ import java.util.Map;
 @Repository
 public interface UserMapper extends BaseMapper<User> {
 
+    @MapKey("id")
     Map<String, Object> selectById(Long id);
 
 }
---
+++ src/test/java/buzz/yixiaobai/MyBatisPlusWrapperTest.java
@@ -0,0 +1,69 @@
+package buzz.yixiaobai;
+
+import buzz.yixiaobai.mapper.UserMapper;
+import buzz.yixiaobai.pojo.User;
+import buzz.yixiaobai.service.UserService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+/**
+ * MyBatisPlus Wrapper 的使用
+ * 条件构建器的使用
+ */
+@SpringBootTest
+public class MyBatisPlusWrapperTest {
+
+    /**
+     * 用户Mapper
+     */
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 用户Service
+     */
+    @Autowired
+    private UserService userService;
+
+    /**
+     *
+     */
+    @Test
+    public void test(){
+        //  SELECT uid AS id,user_name AS name,age,email,is_deleted FROM t_user WHERE is_deleted=0
+        //  AND (user_name LIKE ? AND age BETWEEN ? AND ? AND email IS NOT NULL)
+        QueryWrapper<User> wrapper = new QueryWrapper<>(new User());
+        wrapper.like("user_name","张")
+                .between("age", 20, 25)
+                .isNotNull("email");
+//        wrapper.getEntity().setName("张三");
+        List<User> userList = userMapper.selectList(wrapper);
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void test02(){
+        // 查询用户信息，按照年龄的降序排序，若年龄相同，则按照id升序排序
+        // SELECT uid AS id,user_name AS name,age,email,is_deleted FROM t_user WHERE is_deleted=0 ORDER BY age DESC,uid ASC
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("age")
+                .orderByAsc("uid");
+        List<User> userList = userMapper.selectList(queryWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void test03(){
+        // 删除邮箱为空的数据
+        // UPDATE t_user SET is_deleted=1 WHERE is_deleted=0 AND (email IS NULL)
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.isNull("email");
+        int count = userMapper.delete(queryWrapper);
+        System.out.println("受影响的行数：" + count);
+
+    }
+}
